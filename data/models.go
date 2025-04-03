e168b992c6b39e3377d56ff4bb8ddc2dab8557de
Alex Worrad-Andrews <alex@alexandrews.info>
2025-04-03 12:29:01 +0100

    All datatypes from Story in database are present

--- data/models.go
+++ data/models.go
@@ -21,3 +21,39 @@ type StoryImage struct {
 	Filename        string
 	AlternativeText string
 }
+
+type Story struct {
+	ID                      string
+	CreatedTime             string
+	Finding                 string
+	HighStExperiment        string
+	WhatWasIsIf             string
+	Image                   string
+	SourceImage             string
+	Location                string
+	StartDateTime           string
+	EndDateTime             string
+	Season                  string
+	Weather                 string
+	StreetDetectoristClue   string
+	Themes                  string
+	Experience              string
+	TimeSpan                string
+	OtherComments           string
+	Type                    string
+	PersonFinder            string
+	MapCache                string
+	MapSize                 string
+	Created                 string
+	StreetDetectoristMapURL string
+	OtherTheme              string
+	OtherWeather            string
+	ShareStatus             string
+	PostDate                string
+	TwitterText             string
+	CharacterCount          string
+	InstaText               string
+	InstaCount              string
+	InstaImage              string
+	ImageData               []byte
+}
