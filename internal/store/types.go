6080e19753c95544c6d7f227d491be7fbcecb1ba
Alex Worrad-Andrews <alex@alexandrews.info>
2025-04-03 11:21:49 +0100

    Extract types from the database

--- internal/store/types.go
+++ internal/store/types.go
@@ -2,22 +2,78 @@
 package store
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"community-climate-justice-archive/data"
 )
 
-// GetTypes retrieves all types from the types directory and returns them as a slice of Type.
+// GetTypes retrieves all types from the database and returns them as a slice of Type.
 // Intended for passing to HTML templates.
-// For the moment this is hardcoded, but we will use SQLite to populate this.
 func GetTypes() []data.Type {
 	log.Println("Getting types")
 
-	return []data.Type{
-		{Title: "text"},
-		{Title: "image"},
-		{Title: "video"},
-		{Title: "sound"},
-		{Title: "textile"},
+	dbPath := "airtable-export.db"
+
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
 	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT Themes type FROM stories")
+	if err != nil {
+		log.Fatalf("Failed to query types: %v", err)
+	}
+	defer rows.Close()
+
+	var types []data.Type
+
+	for rows.Next() {
+		var (
+			Theme sql.NullString
+		)
+		if err := rows.Scan(&Theme); err != nil {
+			log.Fatalf("Failed to scan row: %v", err)
+		}
+
+		if Theme.Valid {
+			// First unmarshal into a string array since it's in format ["Tiny Things", "Care"]
+			var themeStrings []string
+			if err := json.Unmarshal([]byte(Theme.String), &themeStrings); err != nil {
+				log.Fatalf("Failed to unmarshal JSON: %v", err)
+			}
+
+			// Convert each theme string into a Type
+			for _, themeStr := range themeStrings {
+				types = append(types, data.Type{Title: themeStr})
+			}
+		}
+	}
+
+	log.Printf("Found %d types", len(types))
+
+	types = uniqueTypes(types)
+	log.Printf("Found %d unique types", len(types))
+
+	return types
+}
+
+// uniqueTypes returns a slice of unique types
+func uniqueTypes(types []data.Type) []data.Type {
+	seen := make(map[string]bool)
+	unique := []data.Type{}
+
+	// Loop over the slice and only keep first occurrence
+	for _, t := range types {
+		if !seen[t.Title] {
+			seen[t.Title] = true
+			unique = append(unique, t)
+		}
+	}
+
+	return unique
 }
