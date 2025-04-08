ed380c49c62e0ceccf5a65e9e0ec3cd5fb608cd8
Alex Worrad-Andrews <alex@alexandrews.info>
2025-04-08 10:56:25 +0100

    Add methods to get images for a given story

--- data/models.go
+++ data/models.go
@@ -1,6 +1,10 @@
 package data
 
-import "database/sql"
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+)
 
 type Page struct {
 	Title       string
@@ -29,6 +33,18 @@ type Type struct {
 type StoryImage struct {
 	Filename        string
 	AlternativeText string
+	Type            string
+	Size            int
+	Width           int
+	Height          int
+	URL             string
+	Thumbnails      map[string]Thumbnail
+}
+
+type Thumbnail struct {
+	URL    string
+	Width  int
+	Height int
 }
 
 type Story struct {
@@ -64,6 +80,57 @@ type Story struct {
 	InstaText               string
 	InstaCount              string
 	InstaImage              string
+}
+
+// GetStoryImage returns the image for a story.
+func (s *Story) GetStoryImages() []StoryImage {
+	// The image is stored as a following JSON blob format from Airtable:
+	// [
+	//   {
+	//     "id": "attENPLcNOsEdYP5E",
+	//     "width": 3252,
+	//     "height": 2193,
+	//     "url": "https://v5.airtableusercontent.com/...",
+	//     "filename": "Michelle Gartside Model 5.png",
+	//     "size": 7205950,
+	//     "type": "image/png",
+	//     "thumbnails": {
+	//       "small": {
+	//         "url": "https://v5.airtableusercontent.com/...",
+	//         "width": 53,
+	//         "height": 36
+	//       },
+	//       "large": {
+	//         "url": "https://v5.airtableusercontent.com/...",
+	//         "width": 759,
+	//         "height": 512
+	//       },
+	//       "full": {
+	//         "url": "https://v5.airtableusercontent.com/...",
+	//         "width": 3000,
+	//         "height": 3000
+	//       }
+	//     }
+	//   }
+	// ]
+
+	var images []StoryImage
+	json.Unmarshal([]byte(s.Image), &images)
+
+	for i := range images {
+		images[i].URL = "/images/" + images[i].Filename
+	}
+
+	return images
+}
+
+func (s *Story) GetStoryImage() StoryImage {
+	images := s.GetStoryImages()
+	if len(images) > 0 {
+		log.Println("Found", len(images), "images for story", images[0].URL)
+		return images[0]
+	}
+	return StoryImage{}
 }
 
 // StoryDTO is a data transfer object that handles NULL values from the database.
