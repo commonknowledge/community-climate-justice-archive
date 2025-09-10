81d0cdde2b1c7a93d70d7f042804c141b08207f0
Alex Worrad-Andrews <alex@alexandrews.info>
2025-09-10 15:18:14 +0100

    Re-enable compilation

--- cmd/archive/main.go
+++ cmd/archive/main.go
@@ -27,55 +27,55 @@ func regenerate(skipImages bool) error {
 	log.Println("Starting build process")
 
 	// Commented out for now while we debug the NocoDB connection
-	// if !skipImages {
-	// 	if err := generate.ProcessImages(); err != nil {
-	// 		return fmt.Errorf("failed to process images: %v", err)
-	// 	}
-	// }
+	if !skipImages {
+		if err := generate.ProcessImages(); err != nil {
+			return fmt.Errorf("failed to process images: %v", err)
+		}
+	}
 
-	// if err := generate.WriteStories(); err != nil {
-	// 	return fmt.Errorf("failed to write stories: %v", err)
-	// }
+	if err := generate.WriteStories(); err != nil {
+		return fmt.Errorf("failed to write stories: %v", err)
+	}
 
-	// if err := generate.WriteHomePage(); err != nil {
-	// 	return fmt.Errorf("failed to write homepage: %v", err)
-	// }
+	if err := generate.WriteHomePage(); err != nil {
+		return fmt.Errorf("failed to write homepage: %v", err)
+	}
 
-	// if err := generate.WriteWanderPage(); err != nil {
-	// 	return fmt.Errorf("failed to write wander page: %v", err)
-	// }
+	if err := generate.WriteWanderPage(); err != nil {
+		return fmt.Errorf("failed to write wander page: %v", err)
+	}
 
-	// if err := generate.WriteArchivePage(); err != nil {
-	// 	return fmt.Errorf("failed to write archive page: %v", err)
-	// }
+	if err := generate.WriteArchivePage(); err != nil {
+		return fmt.Errorf("failed to write archive page: %v", err)
+	}
 
-	// if err := generate.WriteFilterData(); err != nil {
-	// 	return fmt.Errorf("failed to write filter data: %v", err)
-	// }
+	if err := generate.WriteFilterData(); err != nil {
+		return fmt.Errorf("failed to write filter data: %v", err)
+	}
 
-	// if err := generate.WriteTypesIndexes(); err != nil {
-	// 	return fmt.Errorf("failed to write types indexes: %v", err)
-	// }
+	if err := generate.WriteTypesIndexes(); err != nil {
+		return fmt.Errorf("failed to write types indexes: %v", err)
+	}
 
-	// if err := generate.WriteThemesIndexes(); err != nil {
-	// 	return fmt.Errorf("failed to write themes indexes: %v", err)
-	// }
+	if err := generate.WriteThemesIndexes(); err != nil {
+		return fmt.Errorf("failed to write themes indexes: %v", err)
+	}
 
-	// if err := generate.WriteWeatherIndexes(); err != nil {
-	// 	return fmt.Errorf("failed to write weather indexes: %v", err)
-	// }
+	if err := generate.WriteWeatherIndexes(); err != nil {
+		return fmt.Errorf("failed to write weather indexes: %v", err)
+	}
 
-	// if err := generate.CopyImagesToOutput(); err != nil {
-	// 	return fmt.Errorf("failed to copy images: %v", err)
-	// }
+	if err := generate.CopyImagesToOutput(); err != nil {
+		return fmt.Errorf("failed to copy images: %v", err)
+	}
 
-	// if err := generate.CopyCSSToOutput(); err != nil {
-	// 	return fmt.Errorf("failed to copy CSS: %v", err)
-	// }
+	if err := generate.CopyCSSToOutput(); err != nil {
+		return fmt.Errorf("failed to copy CSS: %v", err)
+	}
 
-	// if err := generate.CopyJSToOutput(); err != nil {
-	// 	return fmt.Errorf("failed to copy JavaScript: %v", err)
-	// }
+	if err := generate.CopyJSToOutput(); err != nil {
+		return fmt.Errorf("failed to copy JavaScript: %v", err)
+	}
 
 	if err := generate.WriteNocoDBTestPage(); err != nil {
 		return fmt.Errorf("failed to write NocoDB test page: %v", err)
